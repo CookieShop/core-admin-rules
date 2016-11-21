699f295871485badc8eb4a2485aa804509bf9e69
DEV <dev@aspire>
2016-11-21 06:56:48 -0600

    version 1.0.0

---
+++ src/Layout.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Helper para obtener cabeceras de tabla pmr_rules y generar archivo csv 	
+ * 
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @author Ing. Eduardo Ortiz
+ * 
+ */
+namespace Adteam\Core\Admin\Rules;
+
+class Layout{
+ 
+}
