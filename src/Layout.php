9fb03899090a6abc47a0d8be7fcb2a610f9793cf
dev <dev@aspire>
2016-11-21 13:24:08 -0600

    version 1.0.1

--- src/Layout.php
+++ src/Layout.php
@@ -8,6 +8,87 @@
  */
 namespace Adteam\Core\Admin\Rules;
 
-class Layout{
- 
+use Zend\ServiceManager\ServiceManager;
+use Doctrine\ORM\EntityManager;
+use Application\Entity\PmrRules;
+
+class Layout
+{
+    /**
+     *
+     * @var ServiceManager 
+     */
+    protected $services;
+    
+    /**
+     *
+     * @var EntityManager 
+     */
+    protected $em;
+
+    /**
+     *
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * 
+     * @param ServiceManager $services
+     */
+    public function __construct(ServiceManager $services) 
+    {
+        $this->em = $services->get(EntityManager::class);
+        $this->config = $services->get('config');
+    }
+
+    /**
+     * Genera archivo csv apartir de nombres de columna de tabla
+     * pmr_rules que es customizable
+     * 
+     * @param type $filename
+     * @return type
+     */
+    public function buildFile($filename)
+    {
+        $columns = $this->em->getClassMetadata(PmrRules::class)
+                ->getFieldNames();
+        $newColumns = $this->exeptionColumn($columns);
+        return $this->buildCsv($newColumns, $filename);                
+    }
+    
+    /**
+     * Quita colmnas no necesarias
+     * 
+     * @param type $items
+     * @return type
+     */
+    private function exeptionColumn($items)
+    {
+        $newItems = ['user_id'];
+        foreach ($items as $item){
+            if($item !== 'id'&& $item!=='createdAt'){
+                $newItems[]=$item;
+            }
+        }
+        return $newItems;
+    }
+    
+    /**
+     * Objeto csv
+     * 
+     * @param type $items
+     * @param string $filename
+     * @return string
+     */
+    private function buildCsv($items,$filename)
+    {
+        $filename = $this->config['path'].'/data/upload/csv/'.$filename;
+        $fp = fopen($filename, "w");
+        fputcsv($fp, $items, ',', '"');
+        fclose($fp); 
+        return $filename;
+    }    
+    
+    
 }
